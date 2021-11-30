a6a8ab95406be2b726936536b103b88bfcd2b968
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-11-30 12:45:46 +0600

    dd
    ddd

---
+++ index.js
@@ -0,0 +1,23 @@
+[
+    { 
+        "name":"Web Design",
+        "img":"https://i.ibb.co/h7ybnyL/kisspng-web-development-web-design-logo-website-5abbea0aa79a15-1563172215222645866865-removebg-previ.png",
+        "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptate consequatur repellendus, rerum, adipisci quisquam voluptas quidem nesciunt beatae at quibusdam, ipsa libero iusto! Modi reprehenderit repellendus nesciunt blanditiis vel."
+
+
+    },
+    { 
+        "name":"Web Development",
+        "img":"https://i.ibb.co/h7ybnyL/kisspng-web-development-web-design-logo-website-5abbea0aa79a15-1563172215222645866865-removebg-previ.png",
+        "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptate consequatur repellendus, rerum, adipisci quisquam voluptas quidem nesciunt beatae at quibusdam, ipsa libero iusto! Modi reprehenderit repellendus nesciunt blanditiis vel."
+
+
+    },
+    { 
+        "name":"Responsive Design",
+        "img":"https://i.ibb.co/K0JXV03/150-1506105-responsive-website-desktop-mobile-responsive-design-logo-png-removebg-preview-1.png",
+        "description":" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptate consequatur repellendus, rerum, adipisci quisquam voluptas quidem nesciunt beatae at quibusdam, ipsa libero iusto! Modi reprehenderit repellendus nesciunt blanditiis vel."
+
+
+    },
+]
